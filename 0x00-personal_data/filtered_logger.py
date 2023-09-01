ac2e4d5b145d4e9171cd2b3c86f796d75e5989d2
psych0wh1z <ikeagudike@gmail.com>
2023-09-01 10:08:28 +0100

    Fix database connection values

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,16 +37,17 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Creates a MySQL database connection object"""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    database = os.getenv("PERSONAL_DATA_DB_NAME")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
     db = mysql.connector.connect(
-        host=host,
-        user=username,
-        password=password,
-        database=database
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name
     )
 
     return db
