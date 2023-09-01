d6706140839978e7175e961d19af449f7f21ffd3
psych0wh1z <ikeagudike@gmail.com>
2023-09-01 03:41:10 +0100

    Add format instance method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
 
-from typing import List
+import logging
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -13,3 +14,21 @@ def filter_datum(fields: List[str], redaction: str,
         substitution = f"{field}={redaction}{separator}"
         message = re.sub(pattern, substitution, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize RedactingFormatter class instance"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records"""
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
