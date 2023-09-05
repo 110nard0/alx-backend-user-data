6c49b2eac6df0e79bfe55bee086b908959201da2
psych0wh1z <ikeagudike@gmail.com>
2023-09-05 14:24:15 +0100

    Update BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,15 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth class inherit from Auth class"""
-    def __init__(self):
-        super().__init__()
+    """BasicAuth class inherit from Auth class and implements the
+    Basic HTTP Authentication scheme
+    """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None or type(authorization_header)\
+                is not str or not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ', 1)[1]
