1eb958921438068063749f74a0125636d88c28c0
psych0wh1z <ikeagudike@gmail.com>
2023-09-05 13:41:35 +0100

    Add task6 file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
+
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+from os import getenv
 
 
 app = Flask(__name__)
@@ -63,5 +63,8 @@ if __name__ == "__main__":
     if auth_type == "auth":
         from api.v1.auth.auth import Auth
         auth = Auth()
+    elif auth_type == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
     app.run(host=host, port=port)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class inherit from Auth class"""
+    pass
