313b7e6852614033b7b28f985c00432a27c83f44
psych0wh1z <ikeagudike@gmail.com>
2023-09-05 17:15:36 +0100

    Add task8 files

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,18 +3,31 @@
 BasicAuth class module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """BasicAuth class inherit from Auth class and implements the
-    Basic HTTP Authentication scheme
+    WWW Basic Authentication scheme
     """
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """Returns the Base64 part of the Authorization header
-        for a Basic Authentication
         """
-        if authorization_header is None or type(authorization_header)\
-                is not str or not authorization_header.startswith('Basic '):
+        if authorization_header is None\
+                or type(authorization_header) is not str\
+                or not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """Returns decoded value of a Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None\
+                or type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
