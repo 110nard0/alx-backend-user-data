cc70d7d3e6eddeffb412b5f56803ddd375870d73
psych0wh1z <ikeagudike@gmail.com>
2023-09-06 10:39:15 +0100

    Fix auth instance declaration

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -66,5 +66,4 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", 5000)
-    auth_type = getenv("AUTH_TYPE")
     app.run(host=host, port=port)
