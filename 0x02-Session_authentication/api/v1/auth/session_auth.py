06983d31da7eaa2ff8fe4f3bc0bca5416445b7c4
psych0wh1z <ikeagudike@gmail.com>
2023-09-07 07:50:59 +0100

    Create session_id class method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,10 +5,20 @@ SessionAuth class module for the API
 from api.v1.auth.auth import Auth
 from models.user import User
 from typing import TypeVar
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """SessionAuth class inherits from Auth class and implements the
     WWW Session Authentication scheme
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
