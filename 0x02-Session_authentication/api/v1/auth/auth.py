f6a650175be973c81dcbed77be5307f3271f5184
psych0wh1z <ikeagudike@gmail.com>
2023-09-07 06:52:05 +0100

    Add starter files

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Auth class module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class manages Simple API Authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Confirm path route in list of excluded paths
+        """
+        slashed_path = f'{path}/' if path and not path.endswith('/') else path
+
+        if not path or not excluded_paths:
+            return True
+        for route in excluded_paths:
+            if route.endswith('*') and path.startswith(route[:-1]):
+                return False
+        if path not in excluded_paths and slashed_path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Validate requests to secure API using Auhtorization Header
+        """
+        if request and request.headers.get('Authorization'):
+            return request.headers['Authorization']
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Authenticate current user sending request
+        """
+        return None
