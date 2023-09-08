9e7888d7b1f3abd70bb364770690d9578a0a89d6
psych0wh1z <ikeagudike@gmail.com>
2023-09-08 10:24:43 +0100

    Remove exception from method

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -28,11 +28,10 @@ class SessionDBAuth(SessionExpAuth):
             return None
         UserSession.load_from_file()
         uid = super().user_id_for_session_id(session_id)
-        if uid is None:
-            try:
-                UserSession.search({'session_id': session_id})
-            except Exception:
-                return None
+        if uid is None and not UserSession.all():
+            return None
+        else:
+            pass
         if not UserSession.all():
             return None
         sessions = UserSession.search({'session_id': session_id})
