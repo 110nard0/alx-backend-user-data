39e9ee4c7ae463f139343d5352b5800a4288fb84
psych0wh1z <ikeagudike@gmail.com>
2023-09-12 17:24:04 +0100

    Add update_user DB instance method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,9 +46,9 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Return user with attribute(s) matching keyword argument(s)
+        """Search for user with attribute(s) matching keyword argument(s)
         Args:
-            attributes (dict): a dictionary of attributes to match the user
+            attributes (dict): a dictionary of attributes to match user
         Return:
             Matching user or raised error if user not found
         """
@@ -59,3 +59,44 @@ class DB:
             return user
         except AttributeError:
             raise InvalidRequestError
+
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update found user's attributes
+        Args:
+            user_id (int): user's id
+            kwargs (dict): dictionary of key:value pairs representing the
+                           attributes to update and their corresponding values
+        Return:
+            None
+        """
+        try:
+            user = self._session.query(User).filter(User.id == int(user_id))
+            if user is None:
+                raise NoResultFound
+            for k, v in kwargs.items():
+                if k not in User.__dict__:
+                    raise InvalidRequestError
+                for key in user.__dict__:
+                    if k == key:
+                        setattr(user, k, v)
+        except ValueError:
+            raise
+
+
+
+
+
+
+"""
+In this task, you will implement the DB.update_user method that takes
+as argument a required user_id integer and arbitrary keyword arguments,
+and returns None.
+
+The method will use find_user_by to locate the user to update,
+then will update the user’s attributes as passed in the method’s arguments
+then commit changes to the database.
+
+If an argument that does not correspond to a user attribute is passed,
+raise a ValueError.
+"""
