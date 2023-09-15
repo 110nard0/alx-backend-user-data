4e88b9178b81a56f3b8943fcddce92aba822170d
psych0wh1z <ikeagudike@gmail.com>
2023-09-15 14:35:59 +0100

    Fix route return HTTP status codes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,9 +51,9 @@ def register() -> str:
         return jsonify({
                         "email": user.email,
                         "message": "user created"
-                       })
+                       }), 200
     except ValueError:
-        return jsonify({"message": "email already registered"})
+        return jsonify({"message": "email already registered"}), 400
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
